430c4beac7c7d5fb178c5703b4ebc2a9742fac8c
appert <scoreur@gmail.com>
2021-12-20 22:00:50 +0100

    make anchor, image,article

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,49 @@
 fetch ("http://localhost:3000/api/products")
 .then((reponse) => reponse.json())
-.then((data) =>  addproducts(data))
+.then((data) => addproducts(data))
+
+
+//altTxt: "Photo d'un canapé bleu, deux places"
+//colors: (3) ['Blue', 'White', 'Black']
+//description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+//imageUrl: "http://localhost:3000/images/kanap01.jpeg"
+//name: "Kanap Sinopé"
+//price: 1849
+//_id: "107fb5b75607497b96722bda5b504926"
+
+
+function addproducts(donnees) {
+    const id = donnees[0]._id
+    const imageUrl = donnees[0].imageUrl
+    const altTxt = donnees[0].altTxt
+    const image = makeImage (imageUrl,altTxt)
+    const anchor = makeAnchor(id)
+    const article = makeArticle()
+    article.appendChild(image)
+    appendChildren(anchor, article)
+}
+
+function makeAnchor(id) {
+    const anchor = document.createElement("a")
+    anchor.href = "./product.html?id=" + id
+    return anchor 
+}
+function appendChildren(anchor, article) {
+    const items = document.querySelector("#items")
+    items.appendChild(anchor)
+    anchor.appendChild(article)  
+ }
+ function makeImage(imageUrl, altTxt) {
+     const image = document.createElement("img")
+     image.src = imageUrl
+     image.alt = altTxt
+     return image
+ }
+
+ function makeArticle () {
+     const article = document.createElement("article")
+     return article
+ }
+ 
+
+//console.log(article)
